87483173497d7c48845d3b1b8fd1cbe3a64e2593
NISHANT SHARMA <nishant735123@gmail.com>
2024-02-10 00:37:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState, useRef } from "react"
 function App() {
+  const [scrolled, setScrolled] = useState(0);
+  const divRefs = useRef([]);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY);
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []); console.log(scrolled)
+  useEffect(() => {
+    for (let i = 0; i < 200; i++) {
+      const newDiv = document.createElement('div');
+      newDiv.className = "newdiv";
+      const size = Math.random() * 20;
+      newDiv.style.width = newDiv.style.height = `${size}px`;
+      newDiv.style.position = "absolute";
+      newDiv.style.top = `${Math.random() *2000}%`;
+      newDiv.style.left = `${Math.random() * 100}%`;
+      newDiv.style.zIndex = Math.floor(size); 
+      document.getElementById('root').appendChild(newDiv);
+      divRefs.current.push(newDiv); 
+    }
+  }, []); 
+  const updateTopPosition = () => {
+    divRefs.current.forEach(div => {
+      const currentTop = parseFloat(div.style.top); 
+      const currentzIndex = parseInt(div.style.zIndex); 
+      div.style.top = `${currentTop + (scrolled*currentzIndex)/5000}px`; 
+    
+    });
+  };
+  useEffect(() => {
+  updateTopPosition();
+  }, [scrolled]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
